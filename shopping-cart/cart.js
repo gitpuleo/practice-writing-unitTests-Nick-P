883fdbe993eed8eb5297adfcbd60fdf03273c9df
nfp <nfpuleo@gmail.com>
2025-06-02 13:02:17 -0500

    Tests written and functions functioning

--- shopping-cart/cart.js
+++ shopping-cart/cart.js
@@ -1,7 +1,61 @@
 //define the following methods:
-
-addItem(CaretPosition, item, quantity)
-removeItem(cart, item)
-getTotalItems(cart)
+cart = [];
 
 
+
+//For future: look how messy these functions are, get more comfortable with best practice formatting so it becomes a habit now
+
+function addItem(cart, item, quantity) {
+    if (typeof item !== 'string'){
+    console.log("Item parameter cannot be a number.")
+    return cart;
+    } else if (item.length<1) {
+        console.log(`Blank entries not allowed.`)
+        return cart;
+    } else {
+    while (quantity > 0) {
+    cart.push(item)
+    quantity--;
+    } return cart;
+}};
+
+
+
+function removeItem(cart, item){
+    try {let searchValue = cart.indexOf(item);
+    if (searchValue >= 0){
+    cart.splice(searchValue, 1);
+    return cart;
+    } else {
+        console.log("Indicated item not found in cart or cart parameter is faulty.");
+        return cart;
+    }
+} catch(error) {
+    console.error(error.message);
+} 
+}
+
+
+
+function getTotalItems(cart){
+    if (!Array.isArray(cart)){
+        console.log("Cart type error");
+        return 0;
+    } else {
+    return cart.length;
+    }
+};
+
+addItem(cart, "cheese", 3);
+console.log(cart);
+
+//removeItem(cart, "cheese")
+
+getTotalItems(cart);
+
+
+module.exports = {
+    addItem: addItem,
+    removeItem: removeItem,
+    getTotalItems: getTotalItems
+};
--- shopping-cart/tests/cart.test.js
+++ shopping-cart/tests/cart.test.js
@@ -1,1 +1,127 @@
 //positive, negative, and edge case tests for the three methods
+
+const {addItem, removeItem, getTotalItems} = require('../cart.js'); 
+
+
+describe(`addItem`, function(){
+    //positive test case
+    test(`Should add specified number of items to cart`, function(){
+       const cart = [];
+       const result = addItem(cart, "apple", 1);
+        expect(result).toEqual(['apple']);
+
+    });
+
+    test(`Should add item to cart multiple times`, function(){
+        const cart = [];
+        const result = addItem(cart, "apple", 3);
+        expect(result).toEqual(['apple', 'apple', 'apple']);
+    });
+
+    test(`Does not add items if quantity is zero`, function(){
+        const cart = [];
+        const result = addItem(cart, 'apple', 0);
+        expect(result).toEqual([]);
+    })
+
+    //negative test case
+    test(`String in quantity parameter should add nothing to array`, function(){
+        const cart = [];
+        const result = addItem(cart, "apple", "fry");
+        expect(result).toEqual([]);
+
+    })
+
+    test(`Number in string parameter should...`, function(){
+        const cart = [];
+        const result = addItem(cart, 1, 2);
+        expect(result).toEqual([]);
+
+    })
+
+    test(`Negative number should fail gracefully.`, function(){
+        const cart = [];
+        const result = addItem(cart, 'apple', -2);
+        expect(result).toEqual([]);
+    })
+
+    //Edge Case
+    test(`Empty strings should not be added to the cart`, function(){
+        const cart = [];
+        const result = addItem(cart, "", 44);
+        expect(result).toEqual([]);
+
+    })
+
+});
+
+describe(`removeItem`, function(){
+    //positive case
+    test(`Should remove specified item from cart.`, function(){
+        const cart = ["cheese"];
+        const result = removeItem(cart, "cheese");
+        expect(result).toEqual([]);
+    
+    })
+
+    //negative case
+    test(`Should handle removing from empty cart`, function(){
+        const cart = [];
+        const result = removeItem(cart, "apple");
+        expect(result).toEqual([]);
+    })
+
+     test(`Should continue functioning if no item argument provided`, function(){
+        const cart = [];
+        const result = removeItem(cart);
+        expect(result).toEqual([]);
+
+     })
+    //Edge case
+    test(`Should continue functioning with strange inputs.`, function(){
+        const cart = [];
+        const result = removeItem(cart, null);
+        expect(result).toEqual([]);
+
+     })
+
+     test(`Should continue functioning if cart parameter is funky`, function(){
+        const cart = [];
+        const result = removeItem(null, "apple");
+        expect(result).toEqual([]);
+
+     }) //Why does this test fail? I am not capturing the null error for some reason. 
+
+    });
+
+    describe(`getTotalItems`, function(){
+        //positive case
+        test('Function should return the number of items in the array.', function(){
+            const cart = ['1', '2', '3', '4', '5'];
+            const result = getTotalItems(cart);
+            expect(result).toEqual(5)
+
+         })
+        //negative test
+        test('If cart is not an array, return 0 and print an error.', function(){
+            const cart = [];
+            const result = getTotalItems("not an array");
+            expect(result).toEqual(0);
+
+
+        })
+        //edge cases
+        test('Empty array should return 0', function(){
+            const cart = [];
+            const result = getTotalItems([]);
+            expect(result).toEqual(0);
+        })
+
+        test('Mixed-types array does not cause error to throw and counts correctly.', function(){
+            const cart = [];
+            const result = getTotalItems([1, 'cheese', ['hi', 'bye']]);
+            expect(result).toEqual(3);
+        })
+
+
+    });
